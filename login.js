3730a8f04cdd1c62f476b72fdaa698d1ca3f1716
Piqua <71940340+LAZERPOINT@users.noreply.github.com>
2022-05-05 19:05:52 -0400

    Delete login.js

--- login.js
+++
@@ -1,6 +0,0 @@
-function myFunction(){
-	var usernameusr = document.getElementById("usralias").value;
-	sessionStorage.clear();
-	sessionStorage.setItem("Welcome", usernameusr);
-	window.location.replace("server.html");
-}
